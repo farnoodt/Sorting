a81f4783043d23bda9081068f34879439da1986c
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-28 15:26:05 -0700

    Add project files.

---
+++ Sorting/MergeSort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class MergeSort
+    {
+        public List<int> sort(List<int> A)
+        {
+            int ALen = A.Count;
+            if (ALen <= 1)
+                return A;
+
+            int m = ALen / 2;
+            List<int> L = new List<int>();
+            List<int> R = new List<int>();
+            for (int i = 0; i < m; i++)
+            {
+                L.Add( A[i]);
+            }
+
+            for (int i = m; i < ALen; i++)
+            {
+                R.Add( A[i]);
+            }
+
+            L = sort(L);
+            R = sort(R);
+
+            return Merge(L, R);
+        }
+
+        public List<int> Merge(List<int> L,List<int> R)
+        {
+            List<int> M = new List<int>();
+
+            while (L.Count != 0 || R.Count != 0)
+            {
+                if(L.Count != 0 && R.Count != 0)
+                {
+                    if (L.First() > R.First())
+                    {
+                        M.Add(L.First());
+                        L.RemoveAt(0);
+                    }
+                    else 
+                    {
+                        M.Add(R.First());
+                        R.RemoveAt(0);
+                    }
+                }
+                else if (L.Count != 0)
+                {
+                    M.Add(L.First());
+                    L.RemoveAt(0);
+                }
+                else if(R.Count != 0)
+                {
+                    M.Add(R.First());
+                    R.RemoveAt(0);
+                }
+            }
+            return M;
+        }
+    }
+}
---
+++ Sorting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MergeSort MS = new MergeSort();
+            List<int> L = new List<int>() { 1, 3, 2, 9, 4, 7 };
+            
+
+            Console.WriteLine(string.Join(",", MS.sort(L)));
+        }
+    }
+}
