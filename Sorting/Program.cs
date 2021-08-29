b67e32efa4b240a90ff4ed444ba6d6f430255ab7
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-08-28 20:43:14 -0700

    Add Quick sorting

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sorting
 {
@@ -9,7 +10,12 @@ namespace Sorting
         {
             MergeSort MS = new MergeSort();
             List<int> L = new List<int>() { 1, 3, 2, 9, 4, 7 };
-            
+            int[] Arr =  new int[] { 1, 3, 2, 9, 4, 7 };
+            int len = Arr.Length - 1;
+            QuickSort QS = new QuickSort();
+             QS.Sort(Arr, 0, len);
+            Console.WriteLine(string.Join(",", Arr));
+ 
 
             Console.WriteLine(string.Join(",", MS.sort(L)));
         }
---
+++ Sorting/QuickSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class QuickSort
+    {
+        public void Sort(int[] Arr, int Low, int High)
+        {
+            if (Low < High)
+            {
+                int pi = Partition(Arr, Low, High);
+
+                Sort(Arr, Low, pi - 1);
+                Sort(Arr, pi + 1, High);
+            }
+            
+        }
+
+        private void Swap(int[] Arr, int A, int B)
+        {
+            int temp = Arr[A];
+            Arr[A] = Arr[B];
+            Arr[B] = temp;
+        }
+
+        private int Partition(int[] Arr, int low, int high)
+        {
+            int Pivot = Arr[high];
+            int i = low - 1;
+            for (int j = low; j <= high-1; j++)
+            {
+                if(Arr[j] < Pivot)
+                {
+                    i++;
+                    Swap(Arr, i, j);
+                }
+            }
+            Swap(Arr, i + 1, high);
+            return i + 1;
+        }
+    }
+}
